7583ccede186bc3c6441ca31756438784d481b5f
liuxiaobleach <1241368737@qq.com>
2023-08-16 10:35:24 +0800

    add test

--- fabric/small/main/main.go
+++ fabric/small/main/main.go
@@ -19,14 +19,17 @@ func main() {
 
 	log.Info("Start to setup pk")
 	var pk = groth16.NewProvingKey(ecc.BN254)
-	err = common.ReadProvingKey("test_small_circuit.pk", pk)
-	if err != nil {
-		log.Warnf("Failed to read pk %s, and try create", err.Error())
-		pk, _, err = groth16.Setup(ccs)
+	var vk = groth16.NewVerifyingKey(ecc.BN254)
+	pk, err1 := common.ReadProvingKey("test_small_circuit.pk", pk)
+	vk, err2 := common.ReadVerifyingKey("test_small_circuit.vk", vk)
+	if err1 != nil || err2 != nil {
+		log.Warnf("Failed to read pk and vk, and try create, err:%v %v", err1, err2)
+		pk, vk, err = groth16.Setup(ccs)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		common.WriteProvingKey(pk, "test_small_circuit.pk")
+		common.WriteVerifyingKey(vk, "test_small_circuit.vk")
 	}
 	log.Infoln("pk load done.")
 
@@ -42,9 +45,19 @@ func main() {
 		return
 	}
 
-	_, err = groth16.Prove(ccs, pk, witness)
+	proof, err := groth16.Prove(ccs, pk, witness)
 	if err != nil {
 		log.Errorf("Receipt failed to prove for: %s\n", err.Error())
 		return
+	}
+
+	pubkWitness, err := witness.Public()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = groth16.Verify(proof, vk, pubkWitness)
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
