63c2a9daac2ee3bc02f78caedf06033e89144906
liuxiaobleach <1241368737@qq.com>
2023-08-29 16:14:09 +0800

    update proof

--- fabric/receipt-proof/main/main.go
+++ fabric/receipt-proof/main/main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"github.com/celer-network/brevis-circuits/common"
 	"github.com/celer-network/brevis-circuits/fabric/receipt-proof/core"
+	"github.com/celer-network/brevis-circuits/fabric/receipt-proof/util"
 	"github.com/celer-network/goutils/log"
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
-	groth162 "github.com/consensys/gnark/backend/groth16/bn254"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
-	"github.com/influxdata/influxdb/pkg/deep"
 )
 
 func main() {
+	contractAddr := "0x87c644c9b0bd2c14f0952aed75e242237e7e3510"
+	topic := "0x7ae1420774474a63c6da37d66e70351e80273a7f6a538d8c05d21d727571dded"
+	fromAddr := "0x58b529F9084D7eAA598EB3477Fe36064C5B7bbC1"
+	txHash := "0x78a175f07d49f3d57e35ec40eca2b7e160dc9cf04fa8103812ede1ca128e7149"
+	smtRoot := "0x0000000000000000000000000000000000000000000000000000000000000001"
+	vol := uint64(14)
+	rpc := "https://goerli.blockpi.network/v1/rpc/public"
+
+	assignment, _, err := util.GenerateReceiptSingleNumSumCircuitProofWitness(rpc, smtRoot, txHash, contractAddr, fromAddr, topic, vol)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	ccs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &core.SingleNumSumCircuit{})
 	if err != nil {
 		log.Errorf("Receipt failed to compile for: %s\n", err.Error())
@@ -32,28 +44,16 @@ func main() {
 		}
 		common.WriteProvingKey(pk, "test_single_number_circuit.pk")
 		common.WriteVerifyingKey(vk, "test_single_number_circuit.vk")
-
-		var pkFromDisk = groth16.NewProvingKey(ecc.BN254)
-		err1 := common.ReadProvingKey("test_single_number_circuit.pk", pkFromDisk)
-		if err1 != nil {
-			log.Fatalln(err1)
-		}
-
-		one := pk.(*groth162.ProvingKey)
-		two := pkFromDisk.(*groth162.ProvingKey)
-
-		CompareBN254ProvingKey(one, two)
-
 	}
 
 	log.Infoln("pk load done.")
 
-	/*witness, err := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
+	witness, err := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
 
 	if err != nil {
 		log.Errorf("Receipt failed to setup for: %s\n", err.Error())
 		return
-	}*/
+	}
 
 	// for bench
 	/*var wg sync.WaitGroup
@@ -74,7 +74,7 @@ func main() {
 	wg.Wait()*/
 
 	// for seq test
-	/*for i := 0; i < 20; i++ {
+	for i := 0; i < 20; i++ {
 		log.Infof("bench num: %d", i)
 		proof, err := groth16.Prove(ccs, pk, witness)
 		if err != nil {
@@ -93,12 +93,98 @@ func main() {
 			log.Errorf("Receipt failed to get verify for: %s\n", err.Error())
 			return
 		}
-	}*/
+	}
 
 	log.Infoln("finish prove")
 }
 
-func CompareBN254ProvingKey(one, two *groth162.ProvingKey) {
+/*func main() {
+ccs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &core.SingleNumSumCircuit{})
+if err != nil {
+	log.Errorf("Receipt failed to compile for: %s\n", err.Error())
+	return
+}
+
+log.Info("Start to setup pk")
+var pk = groth16.NewProvingKey(ecc.BN254)
+var vk = groth16.NewVerifyingKey(ecc.BN254)
+err1 := common.ReadProvingKey("test_single_number_circuit.pk", pk)
+err2 := common.ReadVerifyingKey("test_single_number_circuit.vk", vk)
+if err1 != nil || err2 != nil {
+	log.Warnf("Failed to read pk and vk, and try create, %v, %v", err1, err2)
+	pk, vk, err = groth16.Setup(ccs)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	common.WriteProvingKey(pk, "test_single_number_circuit.pk")
+	common.WriteVerifyingKey(vk, "test_single_number_circuit.vk")
+
+	var pkFromDisk = groth16.NewProvingKey(ecc.BN254)
+	err1 := common.ReadProvingKey("test_single_number_circuit.pk", pkFromDisk)
+	if err1 != nil {
+		log.Fatalln(err1)
+	}
+
+	one := pk.(*groth162.ProvingKey)
+	two := pkFromDisk.(*groth162.ProvingKey)
+
+	CompareBN254ProvingKey(one, two)
+
+}
+
+log.Infoln("pk load done.")
+
+/*witness, err := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
+
+if err != nil {
+	log.Errorf("Receipt failed to setup for: %s\n", err.Error())
+	return
+}*/
+
+// for bench
+/*var wg sync.WaitGroup
+log.Infoln("start prove")
+for i := 0; i < 5; i++ {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for x := 0; x < 100; x++ {
+			_, err = groth16.Prove(ccs, pk, witness)
+			if err != nil {
+				log.Errorf("Receipt failed to prove for: %s\n", err.Error())
+				return
+			}
+		}
+	}()
+}
+wg.Wait()*/
+
+// for seq test
+/*for i := 0; i < 20; i++ {
+	log.Infof("bench num: %d", i)
+	proof, err := groth16.Prove(ccs, pk, witness)
+	if err != nil {
+		log.Errorf("Receipt failed to prove for: %s\n", err.Error())
+		return
+	}
+
+	pubWitness, err := witness.Public()
+	if err != nil {
+		log.Errorf("Receipt failed to get pub witness for: %s\n", err.Error())
+		return
+	}
+
+	err = groth16.Verify(proof, vk, pubWitness)
+	if err != nil {
+		log.Errorf("Receipt failed to get verify for: %s\n", err.Error())
+		return
+	}
+}*/
+
+/*	log.Infoln("finish prove")
+}*/
+
+/*func CompareBN254ProvingKey(one, two *groth162.ProvingKey) {
 	//log.Infof("one.IsDifferent(two): %v", one.IsDifferent(two))
 
 	log.Infof("deep.Equal(one.Domain, two.Domain): %v", deep.Equal(one.Domain, two.Domain))
@@ -125,4 +211,4 @@ func CompareBN254ProvingKey(one, two *groth162.ProvingKey) {
 	log.Infof("deep.Equal(one.NbInfinityA, two.NbInfinityA): %v", deep.Equal(one.NbInfinityA, two.NbInfinityA))
 	log.Infof("deep.Equal(one.NbInfinityB, two.NbInfinityB): %v", deep.Equal(one.NbInfinityB, two.NbInfinityB))
 	log.Infof("deep.Equal(one.CommitmentKey, two.CommitmentKey): %v", deep.Equal(one.CommitmentKey, two.CommitmentKey))
-}
+}*/
